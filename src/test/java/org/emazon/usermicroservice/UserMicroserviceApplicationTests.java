3699216ec4a9ad195ea5bd73365b55da3a0fed46
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-08-31 10:23:44 -0500

    first commit

---
+++ src/test/java/org/emazon/usermicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emazon.usermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
